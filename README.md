9e2acc640d918ad3fb61758f30a0d16754e45c78
mvpratt <mvpratt@gmail.com>
2017-07-05 14:41:40 -0600

    add description to readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# piggybank
+# PiggyBank - Answers
+
+In the previous examples, “balance” was a state variable, now let’s send real ether to our contract
+
+Make a PiggyBank.sol contract that accepts ether  and only pays out when a savings goal is reached
+* Set savings goal in the constructor
+* Make a payable deposit() function   
+* Make a withdrawal() function that pays out only once goal is reached
+
+Using the “User Accounts” example and the “Call a contract function” example, deposit ether to 
+the PiggyBank from Alice’s account.  Withdraw once the savings goal is reached.
+
+Bonus
+withdrawal() only pays to the contract creator  (hint - truffle deploys from web3.eth.accounts[0] )
+
+New concepts in this assignment:  tx.origin, msg.sender, address.send  
+Reading: http://solidity.readthedocs.io/en/develop/units-and-global-variables.html
+*/
--- contracts/PiggyBank.sol
+++ contracts/PiggyBank.sol
@@ -1,23 +1,5 @@
 pragma solidity ^0.4.8;
 
-/*
-In the previous examples, “balance” was a state variable, now let’s send real ether to our contract
-
-Make a PiggyBank.sol contract that accepts ether  and only pays out when a threshodl is reached
-Make a payable deposit() function   
-Set threshold in the constructor
-Make a withdrawal() function that pays out only once threshold is reached
-
-Using the “User Accounts” example and the “Call a contract function” example, deposit ether to 
-the PiggyBank from Alice’s account.  Withdraw once the savings threshold is reached.
-
-Bonus
-withdrawal() only pays to the contract creator  (hint - truffle deploys from web3.eth.accounts[0] )
-
-New concepts in this assignment:  tx.origin, msg.sender, address.send  
-Reading: http://solidity.readthedocs.io/en/develop/units-and-global-variables.html
-*/
-
 
 contract PiggyBank {
 
